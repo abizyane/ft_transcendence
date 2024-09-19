6aa3a27e20c1a4fcc16663e043aabbe5b405e9f4
ahmed allali <allali15679@hotmail.com>
2024-09-19 21:24:16 +0100

    add links

--- frontend/app/src/components/Charts/winrate.tsx
+++ frontend/app/src/components/Charts/winrate.tsx
@@ -29,7 +29,7 @@ const DoughnutChart = () => {
   return (
     <div className=" w-[99%] h-[99%] ">
       <div className=" absolute z-50  w-full h-full flex justify-center items-center">
-        <p className="text-white font-mont sm:font-bold text-xs">20%</p>
+        <p className="text-white font-mont sm:font-bold text-xs pt-2">20%</p>
       </div>
       <Doughnut data={data} options={options} />
     </div>
