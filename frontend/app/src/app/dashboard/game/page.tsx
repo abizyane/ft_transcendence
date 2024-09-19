3313ad942e51e0604d13c42b8c5858e39fe9abf8
ahmed allali <allali15679@hotmail.com>
2024-09-19 18:30:28 +0100

    creating first game page

---
+++ frontend/app/src/app/dashboard/game/page.tsx
@@ -0,0 +1,30 @@
+import solo from "../../../../public/solo.jpeg";
+import multiplayers from "../../../../public/multiplayers.jpeg";
+
+const Page = () => {
+    return (
+        <>
+            <div className=" bg-gray-800 bg-opacity-60 rounded-xl flex flex-col gap-4 border-[1px] border-violet-primary  md:gap-6  md: mb-8 w-full justify-center items-center md:flex-row overflow-hidden">
+                <div className="min-w-[200px] max-w-[350px] m-2 h-1/2 flex flex-col rounded-2xl border-4 border-violet-primary">
+                    <div className="h-3/4">
+                        <img src={solo.src} alt="mode solo" className="w-full h-full object-cover rounded-t-xl" />
+                    </div>
+                    <div className="bg-black h-1/4 text-center font-bold text-white rounded-b-xl">
+                        Mode Solo
+                    </div>
+                </div>
+                <div className="bg-gray-500 min-w-[200px] max-w-[350px] h-1/2 flex flex-col rounded-2xl mb-8 md:mb-0 border-4 border-violet-primary">
+                    <div className="h-3/4">
+                        <img src={multiplayers.src} alt="mode multiplayers" className="w-full h-full object-cover rounded-t-xl" />
+                    </div>
+                    <div className="bg-black h-1/4 text-center font-bold text-white rounded-b-xl">
+                        Mode Tournements
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Page;
