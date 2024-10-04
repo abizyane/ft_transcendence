2469d6fed62263baf654a0a6d03a819b55b67980
second <second@MacBook-Pro.local>
2024-10-04 16:41:18 +0100

    chat page creation

---
+++ frontend/app/src/app/chat/layout.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+import "../globals.css";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import Navbar from "@/components/Navbar/Navbar";
+
+interface ChatLayoutProps {
+  children: ReactNode;
+}
+
+export default function Chat({ children }: ChatLayoutProps) {
+  return (
+<div className="w-full min-h-screen flex flex-col justify-start items-start">
+  <div className="w-full h-16"><Navbar /></div>
+
+  {/* Main content area */}
+  <div className="w-full flex lg:flex-row flex-col-reverse flex-grow overflow-hidden">
+    {/* Sidebar section */}
+    <div className="lg:h-screen lg:w-24 w-full z-50 lg:z-0">
+      <Sidebar />
+    </div>
+
+    {/* Main content area */}
+    <div className="w-full flex justify-center items-center">
+      <div className="w-full h-max lg:h-fit flex flex-col justify-center items-center p-2">
+        {children}
+      </div>
+    </div>
+  </div>
+</div>
+
+
+);
+}
+
