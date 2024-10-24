974ea185a48c200309a11ac1dd6f20a52d65af2c
tahaekko <tahaexo@gmail.com>
2024-10-24 14:46:48 +0100

    Fix: building and visualizing tree

--- backend/app/game/Tournament/matchHolder.py
+++ backend/app/game/Tournament/matchHolder.py
@@ -21,8 +21,10 @@ class MatchHolder(Holder):
 
 class PlayerHolder(Holder):
     def __init__(self, competitor):
-        self.competitor = competitor
+        # self.competitor = competitor
         self.back:Holder = None
+        self.left: Holder = None
+        self.right: Holder = None
         self.lvl = 0
         self.index = 0
     pass
@@ -32,38 +34,38 @@ class MatchTreeBuilder(AbstractMatchBuilder):
     #     self.room = match
 
     @staticmethod
-    def build_tree(holder:Holder, index:int ,lvl:int, competitor_generator):
-        if (2 ** lvl == holder.size) :
-            MatchTreeBuilder.build_leafs(holder, lvl, competitor_generator)
-            return holder
+    def build_tree(holder:Holder, index:int ,lvl:int, competitor_generator,size):
         holder.lvl = lvl
         holder.index = index
-        holder.left = MatchHolder()
-        holder.right = MatchHolder()
+        if isinstance(holder, PlayerHolder) :
+            # MatchTreeBuilder.build_leafs(holder, index,lvl + 1, competitor_generator)
+            return holder
+        holder.left = MatchHolder() if (2 ** lvl < size) else PlayerHolder(next(competitor_generator))
+        holder.right = MatchHolder() if (2 ** lvl < size) else PlayerHolder(next(competitor_generator))
         holder.right.back = holder.left.back = holder
-        MatchTreeBuilder.build_tree(holder.left, index + 1, lvl + 1, competitor_generator)
-        MatchTreeBuilder.build_tree(holder.right, index + 2, lvl + 1, competitor_generator)
+        index *= 2
+        MatchTreeBuilder.build_tree(holder.left, index + 1    , lvl + 1, competitor_generator, size)
+        MatchTreeBuilder.build_tree(holder.right, index + 2, lvl + 1, competitor_generator, size)
         return holder
 
     @staticmethod
-    def build_leafs(composite, lvl, competitor_gen) -> None:
-        try:
-            composite.left = next(competitor_gen)
-            composite.left.lvl = lvl + 1
-            composite.right = next(competitor_gen)
-            composite.right.lvl = lvl + 1
-            composite.right.back = composite.left.back = composite
-        except Exception as e:
-            print(e)
+    def build_leafs(composite, index,lvl, competitor_gen) -> None:
+        composite.left = next(competitor_gen)
+        composite.left.lvl = lvl
+        composite.left.index = index + 1
+        composite.right = next(competitor_gen)
+        composite.right.lvl = lvl
+        composite.right.index = index + 2
+        composite.right.back = composite.left.back = composite
         pass
-
+    
     @staticmethod
     def visualize_tree(holder:Holder, lvl, size) -> None:
-        print("%s h:%s" % ((holder.lvl * '\t'), holder.index))
+        print(f"{holder.lvl * '\t'}{'m' if isinstance(holder, MatchHolder) else 'p'}:{holder.index}")
         if (2 ** lvl) >= size :
             return
-        MatchTreeBuilder.visualize_tree(holder.left, lvl + 1, size)
-        MatchTreeBuilder.visualize_tree(holder.right, lvl + 1, size)
+        MatchTreeBuilder.visualize_tree(holder.left, lvl + 1,size)
+        MatchTreeBuilder.visualize_tree(holder.right, lvl + 1,size)
         pass
         
 
