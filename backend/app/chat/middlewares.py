ee1d3b7948572c0d42b3923e2a41da96867764c9
Achraf <abizyane@student.1337.ma>
2024-11-07 22:35:08 +0100

    Fixed: chat APIs fixed

---
+++ backend/app/chat/middlewares.py
@@ -0,0 +1,52 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+from django.contrib.auth.models import AnonymousUser
+from channels.middleware.base import BaseMiddleware
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@database_sync_to_async
+def get_user_from_token(token):
+    try:
+        access_token = AccessToken(token)
+        user_id = access_token['user_id']
+        return User.objects.get(id=user_id)
+    except (TokenError, User.DoesNotExist):
+        return AnonymousUser()
+
+class JWTAuthWebSocketMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        cookies = dict(scope['headers'])
+        access_token = None
+        refresh_token = None
+
+        for header in cookies:
+            if header[0] == b'cookie':
+                cookie_str = header[1].decode()
+                cookies = dict(item.split("=") for item in cookie_str.split("; "))
+                access_token = cookies.get('access')
+                refresh_token = cookies.get('refresh')
+
+        if access_token:
+            user = await get_user_from_token(access_token)
+            if user.is_authenticated:
+                scope['user'] = user
+            else:
+                if refresh_token:
+                    try:
+                        new_access_token = str(RefreshToken(refresh_token).access_token)
+                        user = await get_user_from_token(new_access_token)
+                        if user.is_authenticated:
+                            scope['user'] = user
+                    except TokenError:
+                        scope['user'] = AnonymousUser()
+                else:
+                    scope['user'] = AnonymousUser()
+        else:
+            scope['user'] = AnonymousUser()
+
+        return await super().__call__(scope, receive, send)
--- backend/app/chat/views.py
+++ backend/app/chat/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, authentication
+from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
@@ -6,7 +7,6 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Message
 from astropong.models.UserModel import User, Relationship
 from .serializers import ConversationSerializer, ChatRoomSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticated
 
 class ConversationsPageNumberPagination(PageNumberPagination):
     page_size = 7
@@ -14,94 +14,69 @@ class MessagesPageNumberPagination(PageNumberPagination):
     page_size = 5
 
 class ConversationsView(generics.ListAPIView):
-    queryset = Message.objects.order_by('-timestamp')
+    # queryset = Message.objects.order_by('-timestamp')
     serializer_class = ConversationSerializer
     pagination_class = ConversationsPageNumberPagination
 
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
 
-# class ConversationsView(views.APIView):
-#     def get(self, request):
-#         paginator = ConversationsPageNumberPagination()
-#         messages = Message.objects.order_by('-timestamp')
-#         context = paginator.paginate_queryset(messages, request)
-#         serializer = ConversationSerializer(context, many=True)
-#         return paginator.get_paginated_response(serializer.data)
+    def get_queryset(self, current_user):
+        try:
+            user = User.objects.get(username=current_user)
+        except User.DoesNotExist:
+            raise NotFound("User not found.")
+        return Message.objects.filter(Q(sender=user) | Q(receiver=user)).order_by('-timestamp')
+    
+    # queryset = get_queryset
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({'error': 'You must be authenticated to access this resource.'}, status=401)
+        current_user = request.user.username
+        super().queryset = self.get_queryset(current_user)
 
-
-# class MessagesView(views.APIView):
-#     def get_messages(self, request, sender, receiver):
-#         try:
-#             sender = User.objects.get(username=sender)
-#             receiver = User.objects.get(username=receiver)
-#         except User.DoesNotExist:
-#             raise NotFound("User not found.")
-
-#         if not Relationship.objects.filter(Q(user1=sender, user2=receiver) | Q(user1=receiver, user2=sender), status=Relationship.Status.FRIEND).exists():
-#             raise NotFound("These users are not friends.")
-
-#         messages = Message.objects.filter(Q(sender=sender, receiver=receiver) | Q(sender=receiver, receiver=sender)).order_by('timestamp')
-#         paginator = MessagesPageNumberPagination()
-#         context = paginator.paginate_queryset(messages, request)
-#         serializer = ConversationSerializer(context, many=True)
-#         return paginator.get_paginated_response(serializer.data)
-
-#     def get(self, request, sender, receiver):
-#         try :
-#             messages = self.get_messages(request, sender, receiver)
-#             serializer = MessageSerializer(messages, many=True)
-#             return Response({'messages': serializer.data})
-#         except NotFound as e:
-#             return Response({'error': str(e)}, status=404)
-
-# class ChatRoomView(MessagesView):
-#     def get(self, request, sender, receiver):
-#         try:
-#             paginated_response = self.get_messages(request, sender, receiver)
-#         except Http404 as e:
-#             return Response({'error': str(e)}, status=404)
-
-#         messages = paginated_response.data.get('results', [])
-#         if not messages:
-#             return Response({'error': 'No messages found between the specified users.'}, status=404)
-#         serializer = ChatRoomSerializer({'sender': messages[0]['sender'], 'receiver': messages[0]['receiver'], 'messages': messages})
-#         return Response({
-#             'chatroom': serializer.data,
-#             'next': paginated_response.data.get('next'),
-#             'previous': paginated_response.data.get('previous')
-#         })
+        return super().list(request, *args, **kwargs)
+ 
 
 class ChatRoomView(generics.ListAPIView):
     serializer_class = ChatRoomSerializer
     pagination_class = MessagesPageNumberPagination
 
-    permission_classes = [IsAuthenticated]
-    def get_queryset(self):
-        l_user_name = self.kwargs['l_user']
-        r_user_name = self.kwargs['r_user']
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self, current_user):
+        other_user = self.kwargs['username']
         try:
-            l_user = User.objects.get(username=l_user_name)
-            r_user = User.objects.get(username=r_user_name)
+            currentuser = User.objects.get(username=current_user)
+            otheruser = User.objects.get(username=other_user)
         except User.DoesNotExist:
             raise NotFound("User not found.")
 
-        if not Relationship.objects.filter(Q(user1=l_user, user2=r_user) | Q(user1=r_user, user2=l_user), status=Relationship.Status.FRIEND).exists():
+        if not Relationship.objects.filter(Q(user1=currentuser, user2=otheruser) | Q(user1=otheruser, user2=currentuser), status = Relationship.Status.BLOCKED).not_exists():
             raise NotFound("These users are not friends.")
 
-        return Message.objects.filter(Q(sender=l_user, receiver=r_user) | Q(sender=r_user, receiver=l_user)).order_by('timestamp')
+        return Message.objects.filter(Q(sender=currentuser, receiver=otheruser) | Q(sender=otheruser, receiver=currentuser)).order_by('timestamp')
 
     # queryset = get_queryset
+
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        paginated_response = self.paginate_queryset(queryset)
-        if not paginated_response:
-            return NotFound("No messages found between the specified users.")
-        serializer = ChatRoomSerializer({'l_user': paginated_response[0].sender, 'r_user': paginated_response[0].receiver, 'messages': paginated_response})
-        return Response({
-            'chatroom': serializer.data,
-            'next': self.get_paginated_response(paginated_response).data.get('next'),
-            'previous': self.get_paginated_response(paginated_response).data.get('previous')
-        })
+        if not request.user.is_authenticated:
+            return Response({'error': 'You must be authenticated to access this resource.'}, status=401)
+        current_user = request.user.username
+        super().queryset = self.get_queryset(current_user)
+
+        return super().list(request, *args, **kwargs)
+    #     queryset = self.get_queryset()
+    #     paginated_response = self.paginate_queryset(queryset)
+    #     if not paginated_response:
+    #         return NotFound("No messages found between the specified users.")
+    #     serializer = ChatRoomSerializer({'l_user': paginated_response[0].sender, 'r_user': paginated_response[0].receiver, 'messages': paginated_response})
+    #     return Response({
+    #         'chatroom': serializer.data,
+    #         'next': self.get_paginated_response(paginated_response).data.get('next'),
+    #         'previous': self.get_paginated_response(paginated_response).data.get('previous')
+    #     })
 
 class OnlineUsersPageNumberPagination(PageNumberPagination):
     page_size = 5
@@ -111,5 +86,9 @@ class OnlineUsersView(generics.ListAPIView):
     serializer_class = UserSerializer
     pagination_class = OnlineUsersPageNumberPagination
 
-    permission_classes = [IsAuthenticated]
-    
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({'error': 'You must be authenticated to access this resource.'}, status=401)
+        return super().list(request, *args, **kwargs)
