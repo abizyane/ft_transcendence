9575a8a136e6282b7af535f7f5b0862144f89aee
AhmedAllali <allali156789@hotmail.com>
2024-11-11 21:35:31 +0100

    PUSHI

--- frontend/app/src/app/(pages)/chat/[id]/page.tsx
+++ frontend/app/src/app/(pages)/chat/[id]/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { useUser } from "@/services/context/usercontext";
+import { useParams } from "next/navigation";
 import React, { useState, useEffect } from "react";
 
 const UserChatPage = ({ currentUser, chatUser }) => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
   const { username, profile_pic_url, is_online, id } = currentUser;
-  useEffect(()=>{
-      setMessages(chatUser.messages)
+  useEffect(() => {
+    setMessages(chatUser.messages);
   });
 
   // Function to handle sending a message
@@ -22,62 +23,57 @@ const UserChatPage = ({ currentUser, chatUser }) => {
       setInput(""); // Clear the input field after sending the message
     }
   };
-  console.log('test')
+  console.log("test");
   return (
     <div className="h-full">
       <main className="flex-grow flex flex-row min-h-full">
         <section className="flex flex-col flex-auto border-l border-gray-800">
-          
           {/* Chat header with user details */}
-          <div className="chat-header px-6 py-4 flex bg-gray-800/60 rounded-tl-xl flex-row flex-none justify-between items-center shadow">
-            <div className="flex">
-              <div className="w-12 h-12 mr-4 relative flex flex-shrink-0">
-                <img
-                  className="shadow-md rounded-full w-full h-full object-cover"
-                  src={chatUser.user.profile_pic} // Display the other user's profile picture
-                  alt={chatUser.user.username} // Alt text should be the other user's username
-                />
-              </div>
-              <div className="text-sm">
-                <p className="font-bold">{chatUser.user.username}</p>
-                <p>{is_online ? "Online" : "Offline"}</p>
-              </div>
-            </div>
-          </div>
 
           {/* Chat body displaying messages */}
           <div className="chat-body p-4  h-[630px] overflow-y-scroll">
-            {messages.map((msg, index) => (
-              <div
-                key={index}
-                className={`flex ${
-                  msg.sender === currentUser.username ? "justify-end" : "justify-start"
-                }`}
-              >
+            {messages
+              .slice(0)
+              .reverse()
+              .map((msg, index) => (
                 <div
-                  className={`messages text-sm ${
-                    msg.sender === currentUser.username ? "text-white" : "text-gray-700"
-                  } grid grid-flow-row gap-2`}
+                  key={index}
+                  className={`flex ${
+                    msg.sender === currentUser.username
+                      ? "justify-end"
+                      : "justify-start"
+                  }`}
                 >
                   <div
-                    className={`flex items-center ${
-                      msg.sender === currentUser.username ? "flex-row-reverse" : ""
-                    }`}
+                    className={`messages text-sm ${
+                      msg.sender === currentUser.username
+                        ? "text-white"
+                        : "text-gray-700"
+                    } grid grid-flow-row gap-2`}
                   >
-                    <p
-                      className={`px-6 py-3 m-1 rounded-full ${
-                        msg.sender === currentUser.username ? "bg-violet-primary" : "bg-white"
-                      } max-w-xs lg:max-w-md`}
+                    <div
+                      className={`flex items-center ${
+                        msg.sender === currentUser.username
+                          ? "flex-row-reverse"
+                          : ""
+                      }`}
                     >
-                      {msg.message} {/* Render the message text */}
-                    </p>
+                      <p
+                        className={`px-6 py-3 m-1 rounded-full ${
+                          msg.sender === currentUser.username
+                            ? "bg-violet-primary"
+                            : "bg-white"
+                        } max-w-xs lg:max-w-md`}
+                      >
+                        {msg.message}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
             <p className="p-4 text-center text-sm text-gray-500">
               {messages.length
-                ? messages[messages.length - 1].timestamp // Show the timestamp of the last message
+                ? messages[messages.length - 1].timestamp
                 : "No messages yet"}
             </p>
           </div>
@@ -109,13 +105,12 @@ const UserChatPage = ({ currentUser, chatUser }) => {
   );
 };
 
-
-
 // import React, { useState, useEffect } from "react";
 // import { useUser } from "path-to-useUser-hook"; // Assuming this hook gives you the current user
 // import UserChatPage from "./UserChatPage"; // Assuming this is the chat page component
 
 export default function Page() {
+  const chatUserid = useParams();
   const { user: currentUser } = useUser();
   const [chatUser, setChatUser] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -124,16 +119,20 @@ export default function Page() {
   useEffect(() => {
     const fetchChatUser = async () => {
       try {
-        const response = await fetch("http://localhost:8000/chat/room/testt", {
-          method: "GET",
-          credentials: "include",
-        });
+        const response = await fetch(
+          `http://localhost:8000/chat/room/${chatUserid.id}`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
 
         if (!response.ok) {
           throw new Error("User not found or API error");
         }
 
         const data = await response.json();
+        console.log("here", data);
         setChatUser(data);
       } catch (err) {
         setError(err.message);
--- frontend/app/src/app/(pages)/chat/layout.tsx
+++ frontend/app/src/app/(pages)/chat/layout.tsx
@@ -1,20 +1,18 @@
 "use client";
 
 import React, { ReactNode, useEffect, useState } from "react";
-import "../../globals.css";
-import Sidebar from "@/components/Sidebar/Sidebar";
-import Navbar from "@/components/Navbar/Navbar";
-import Link from "next/link";
+import { useRouter } from "next/navigation"; // Ensure you're using the correct import for useRouter
 import { formatDistanceToNow } from "date-fns";
-import { useRouter } from "next/router";
 import { FiPlusCircle } from "react-icons/fi";
 import { FaSearch } from "react-icons/fa";
+import Link from "next/link"; // Don't forget the import for Link component
+
 interface ChatLayoutProps {
   children: ReactNode;
 }
 
 interface User {
-  id:number;
+  id: number;
   username: string;
   profile_pic: string;
   message: string;
@@ -24,22 +22,24 @@ interface User {
 export default function Chat({ children }: ChatLayoutProps) {
   const [users, setUsers] = useState<User[]>([]);
   const [isSliderOpen, setIsSliderOpen] = useState(false);
-  const [selectedUser, setSelectedUser] = useState<User | null>(null);
-
+  const [selectedId, setSelectedId] = useState<User | undefined>();
+  const [isMobile, setIsMobile] = useState(false); // Track if the user is on a mobile device
+  const router = useRouter();
+  const is_online = true;
+  // Fetch conversation data from the backend
   const fetchConversation = async () => {
     try {
-      const response = await fetch(`http://localhost:8000/chat/conversations`,{
-        credentials: 'include',
+      const response = await fetch(`http://localhost:8000/chat/conversations`, {
+        credentials: "include",
       });
       if (!response.ok) {
-        console.error('Fetch error:', error);
+        console.error("Fetch error:", error);
       }
       const data = await response.json();
-      console.log(data.results);
       setUsers(() => {
         return data?.results.map((User: any) => {
           return {
-            id:User.id,
+            id: User.id,
             username: User.username,
             profile_pic: User.profile_pic,
             message: User.message,
@@ -49,42 +49,71 @@ export default function Chat({ children }: ChatLayoutProps) {
           };
         });
       });
-      return data?.results;
     } catch (error) {
       console.log("Fetch error:", error);
     }
   };
+
   useEffect(() => {
     fetchConversation();
+
+    // Check if the window size is mobile or desktop
+    const handleResize = () => {
+      if (window.innerWidth <= 1024) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    // Add resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Initial check
+    handleResize();
+
+    // Cleanup the event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
+  // Open the sidebar (slide-out) for mobile
   const openSlider = (user: User) => {
-    setSelectedUser(user);
+    setSelectedId(user);
     setIsSliderOpen(true);
   };
 
+  useEffect(() => {
+    console.log(selectedId);
+  }, [selectedId]);
+  // Close the slider
   const closeSlider = () => {
     setIsSliderOpen(false);
-    setSelectedUser(null);
+    router.push("/chat");
   };
 
-    const openChat = (user) => {
-      router.push(`/chat/${user.id}`)
-    }
-  const currentUserId = 2;
+  const handleUserClick = (user: User) => {
+    if (isMobile)
+      openSlider(user);
+    else
+      setSelectedId(user);
+    router.push(`/chat/${user.id}`);
+  };
+
   return (
-    <div className="w-full flex flex-col justify-start items-start ">
-      <div className="w-full flex lg:flex-row h-full  flex-col-reverse">
-        <div className="w-full  h-full ">
-          <div className="w-full  h-full lg:h-full flex flex-col justify-center items-center p-2 ">
-            <div className=" bg-gray-800/60 h-[800px]  w-full  text-gray-200 rounded-xl border-2 border-violet-primary flex">
-              <div className="w-full lg:w-96 backdrop-blur-md  rounded-xl">
+    <div className="w-full flex flex-col justify-start items-start">
+      <div className="w-full flex lg:flex-row h-full flex-col-reverse">
+        {/* Main content */}
+          <div className="w-full h-full lg:h-full flex flex-col justify-center items-center p-2">
+            <div className="bg-gray-800/60 h-[800px] w-full text-gray-200 rounded-xl border-2 border-violet-primary flex">
+              <div className="w-full lg:w-96 backdrop-blur-md rounded-xl">
                 <section className="w-full">
-                  <div className="header p-4  rounded-xl flex justify-between items-center w-full">
+                  <div className="p-4 rounded-xl flex justify-between items-center w-full">
                     <p className="text-md font-bold">Messages</p>
-                    <div className="rounded-full  bg-violet-primary w-10 h-10 flex justify-center items-center">
+                    <div className="rounded-full bg-violet-primary w-10 h-10 flex justify-center items-center">
                       <button className="text-sm">
-                      <FiPlusCircle className="w-8 h-8 rounded-full hover:text-gray-700" />
+                        <FiPlusCircle className="w-8 h-8 rounded-full hover:text-gray-700" />
                       </button>
                     </div>
                   </div>
@@ -100,116 +129,105 @@ export default function Chat({ children }: ChatLayoutProps) {
                             placeholder="Search Messages"
                           />
                           <span className="absolute top-0 left-0 mt-3 ml-3 inline-block">
-                            <FaSearch className="w-5 h-5 text-gray-400"/>
+                            <FaSearch className="w-5 h-5 text-gray-400" />
                           </span>
                         </label>
                       </div>
                     </form>
                   </div>
 
-                  {/* Online User Items */}
-                  {/* <div className="flex flex-row p-2 overflow-auto">
-                    <div className="flex flex-row overflow-auto">
-                      {users &&
-                        users?.map((user) => (
-                          <Link
-                            // key={`chat-${user.id}`}
-                            href={`/chat/${user.id}`}
-                            className="text-sm text-center mr-4 relative"
-                          >
-                            <div className="relative">
-                              <img
-                                className="shadow-md rounded-full w-20 h-20 object-cover"
-                                src={"http://localhost:8000" + user.img}
-                                alt={user.name}
-                              />
-                              <span className="absolute bottom-1 right-1 w-4 h-4 bg-green-500 border-2 border-white rounded-full"></span>
-                            </div>
-                            <p>{user.name}</p>
-                          </Link>
-                        ))}
-                    </div>
-                  </div> */}
-                </section>
-                   {/* <Link key={`message-${user.id}`} href="">
-                  </Link> */}
-               <div className="p-2 flex-1 md:w-full h-[600px] overflow-y-scroll">
-      {users
-          .map((user, i) => (
-            <Link 
-              key={`message-${user.id}`}
-              href={`/chat/${user.id}`}
-              passHref
-            >
-              <div className="flex justify-between items-center p-3 hover:bg-gray-800 rounded-lg cursor-pointer">
-                <div className="w-16 h-16 flex-shrink-0">
-                  <img
-                    className="shadow-md rounded-full w-full h-full object-cover"
-                    src={user.profile_pic}
-                    alt={user.username}
-                  />
-                </div>
-                <div className="flex-auto min-w-0 ml-4 mr-6">
-                  <p className="font-bold">{user.username}</p>
-                  <div className="flex justify-between items-center text-sm text-gray-600">
-                    <p className="truncate">{user.message}</p>
-                    <p className="ml-4 text-white-primary whitespace-nowrap">
-                      {user.time}
-                    </p>
+                  {/* User list */}
+                  <div className="p-2 flex-1 md:w-full h-[600px] overflow-y-scroll">
+                    {users.map((user) => (
+                      <div
+                        key={`message-${user.id}`}
+                        onClick={() => handleUserClick(user)}
+                        className="flex justify-between items-center p-3 hover:bg-gray-800 rounded-lg cursor-pointer"
+                      >
+                        <div className="w-16 h-16 flex-shrink-0">
+                          <img
+                            className="shadow-md rounded-full w-full h-full object-cover"
+                            src={user.profile_pic}
+                            alt={user.username}
+                          />
+                        </div>
+                        <div className="flex-auto min-w-0 ml-4 mr-6">
+                          <p className="font-bold">{user.username}</p>
+                          <div className="flex justify-between items-center text-sm text-gray-600">
+                            <p className="truncate">{user.message}</p>
+                            <p className="ml-4 text-white-primary whitespace-nowrap">
+                              {user.time}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                </div>
+                </section>
               </div>
-            </Link>
-          ))}
-    </div>
 
-                {/* Slide Component */}
+              {/* Main Content (Children) */}
+              <div className="flex-1 hidden lg:block">
+                {selectedId && (
+                  <div className="chat-header px-6 py-4 flex bg-gray-800/60 rounded-tl-xl flex-row flex-none justify-start gap-4 items-center shadow">
+                    <button
+                      onClick={closeSlider}
+                      className="text-gray-600 hover:text-black"
+                    >
+                      Close
+                    </button>
+                    <div className="flex">
+                      <div className="w-12 h-12 mr-4 relative flex flex-shrink-0">
+                        <img
+                          className="shadow-md rounded-full w-full h-full object-cover"
+                          src={selectedId.profile_pic} // Display the other user's profile picture
+                          alt={selectedId.username} // Alt text should be the other user's username
+                        />
+                      </div>
+                      <div className="text-sm">
+                        <p className="font-bold">{selectedId.username}</p>
+                        <p>{is_online ? "Online" : "Offline"}</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {children}
               </div>
-              <div className="flex-1 hidden lg:block">{children}</div>
             </div>
           </div>
-        </div>
+      </div>
+
+      {/* Slide-out Sidebar (mobile) */}
+      <div
+        className={`lg:hidden fixed  bg-gray-800 h-[800px] w-[95%] sm:w-[97%] m-2  text-gray-200 rounded-xl border-2 border-violet-primary  transition-transform transform ${
+          isSliderOpen ? "translate-x-0" : "translate-x-[110%]"
+        }`}
+      >
+        {selectedId && (
+          <div className="chat-header px-6 py-4 flex bg-gray-800/60 rounded-tl-xl flex-row flex-none justify-start gap-4 items-center shadow">
+            <button
+              onClick={closeSlider}
+              className="text-gray-600 hover:text-black"
+            >
+              Close
+            </button>
+            <div className="flex">
+              <div className="w-12 h-12 mr-4 relative flex flex-shrink-0">
+                <img
+                  className="shadow-md rounded-full w-full h-full object-cover"
+                  src={selectedId.profile_pic} // Display the other user's profile picture
+                  alt={selectedId.username} // Alt text should be the other user's username
+                />
+              </div>
+              <div className="text-sm">
+                <p className="font-bold">{selectedId.username}</p>
+                <p>{is_online ? "Online" : "Offline"}</p>
+              </div>
+            </div>
+          </div>
+        )}
+        {children}
       </div>
     </div>
   );
 }
-                // <div
-                //   className={`lg:hidden fixed top-0 right-0 h-full w-full bg-gray-800 rounded-xl backdrop-blur-3xl transition-transform transform ${
-                //     isSliderOpen ? "translate-x-0" : "translate-x-full"
-                //   }`}
-                // >
-                //   <div className="flex justify-start  p-4 border-b">
-                //     <button
-                //       onClick={closeSlider}
-                //       className="text-gray-600 hover:text-black"
-                //     >
-                //       Close
-                //     </button>
-                //     <div className="flex flex-col text-center w-full">
-                //       <h2 className="pl-2 text-lg font-semibold">
-                //         {selectedUser?.name}
-                //       </h2>
-                //       <p>Active 1h ago</p>
-                //     </div>
-                //   </div>
-                //   <div className="flex-1 overflow-y-auto p-4">
-                //     {/* Messages go here */}
-                //   </div>
-                //   <div className="chat-footer w-full fixed bottom-0 left-0 rounded-xl p-4 bg-gray-800 border-t border-gray-700">
-                //     <div className="relative">
-                //       <label className="flex items-center w-full">
-                //         <input
-                //           className="rounded-full py-2 pl-3 pr-20 w-full border border-gray-800 focus:border-gray-700 bg-gray-800  focus:bg-gray-900 focus:outline-none text-gray-200 focus:shadow-md"
-                //           type="text"
-                //           placeholder="Write your message"
-                //         />
-                //         <button
-                //           type="button"
-                //           className="absolute top-1/2 transform -translate-y-1/2 right-4 focus:outline-none text-violet-primary hover:text-blue-700 px-4 py-1"
-                //         >
-                //           Send
-                //         </button>
-                //       </label>
-                //     </div>
-                //   </div>
-                // </div>
