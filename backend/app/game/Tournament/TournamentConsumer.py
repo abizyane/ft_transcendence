052815bc6087b98405eca3b59894328ec1982c21
Achraf <abizyane@student.1337.ma>
2024-11-25 12:03:10 +0100

    Fixed: a small merge error in the tournament consumer fixed

--- backend/app/game/Tournament/TournamentConsumer.py
+++ backend/app/game/Tournament/TournamentConsumer.py
@@ -107,7 +107,6 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         print(self.match.state)
         await self.channel_layer.group_send(self.match_name,{
             'type' : 'finalize.match'
-            'type' : 'finalize.match'
         })
 
     async def finalize_match(self, event):
