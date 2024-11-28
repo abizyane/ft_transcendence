2897dd8df675fed09049f7521dbc0b313bbfff21
AhmedAllali <allali156789@hotmail.com>
2024-11-28 14:28:03 +0100

    update histoory

--- frontend/app/src/app/(pages)/game/tournaments/page.tsx
+++ frontend/app/src/app/(pages)/game/tournaments/page.tsx
@@ -1,4 +1,5 @@
-'use client'; 
+'use client'; // Ensure this is at the top for Next.js 13+ with the `app` directory
+
 import Profil from "../../../../../public/Profil.jpg";
 import { useState } from 'react';
 
@@ -10,6 +11,12 @@ const Page = () => {
   const [availableTournaments, setAvailableTournaments] = useState([
     { name: 'Tournament 1', img: '/tournament1.jpg' },
     { name: 'Tournament 2', img: '/tournament2.jpg' },
+    { name: 'Tournament 3', img: '/tournament2.jpg' },
+    { name: 'Tournament 4', img: '/tournament2.jpg' },
+    { name: 'Tournament 5', img: '/tournament2.jpg' },
+    { name: 'Tournament 6', img: '/tournament2.jpg' },
+    { name: 'Tournament 7', img: '/tournament2.jpg' },
+    { name: 'Tournament 8', img: '/tournament2.jpg' },
   ]);
 
   const handleCreateTournament = (e) => {
@@ -23,8 +30,9 @@ const Page = () => {
 
   return (
     <div className="flex flex-col justify-start items-center px-4 w-full h-full py-8">
+      {/* Alias Zone (Initially at the top) */}
       {!confirmation ? (
-        <div className=" bg-gray-700/50 shadow-lg border border-violet-primary max-w-md p-6 rounded-lg mb-8">
+        <div className="bg-gray-700/50 shadow-lg border border-violet-primary max-w-lg w-full p-6 rounded-lg mb-8">
           <h1 className="text-xl text-white font-semibold text-center">Enter Your Alias</h1>
           <input
             type="text"
@@ -41,9 +49,10 @@ const Page = () => {
         </div>
       ) : (
         <div className="w-full h-full flex justify-center items-center">
-          <div className="max-w-md lg:max-w-7xl p-4 rounded-lg shadow-lg mt-8">
+          <div className="w-full max-w-7xl p-4 rounded-lg shadow-lg mt-8">
             <div className="w-full lg:flex lg:gap-8">
-              <div className={`bg-gray-700/50 shadow-lg border border-violet-primary rounded-lg p-6 max-w-xs lg:min-w-[350px] h-fit lg:min-h-[500px] flex flex-col  space-y-6 ${!confirmation && 'opacity-50 pointer-events-none'}`}>
+              {/* Create Tournament Card */}
+              <div className="bg-gray-700/50 shadow-lg border border-violet-primary rounded-lg p-6 w-full lg:max-w-2xl lg:min-w-[350px] h-[500px] flex flex-col space-y-6">
                 <h2 className="text-lg text-white text-center font-semibold mb-4">Create Tournament</h2>
                 <form onSubmit={handleCreateTournament} className="flex flex-col space-y-6 h-full">
                   <input
@@ -53,12 +62,14 @@ const Page = () => {
                     onChange={(e) => setTournamentName(e.target.value)}
                     className="w-full p-3 border rounded-md bg-gray-500/40 text-white"
                   />
-                  <input
-                    type="file"
-                    onChange={(e) => setTournamentImage(URL.createObjectURL(e.target.files[0]))}
-                    className="w-full p-3 border rounded-md bg-gray-500/40 lg:mb-48"
-                  />
-                  <div className="flex items-center space-x-2 lg:mt-64 ">
+                  <div className="flex justify-center items-center w-full">
+                    <input
+                      type="file"
+                      onChange={(e) => setTournamentImage(URL.createObjectURL(e.target.files[0]))}
+                      className="w-24 h-24 p-3 border rounded-full bg-gray-500/40"
+                    />
+                  </div>
+                  <div className="flex items-center space-x-2 mt-4">
                     <input
                       type="checkbox"
                       checked={isPrivate}
@@ -67,26 +78,25 @@ const Page = () => {
                     />
                     <label className="text-white">Private Tournament</label>
                   </div>
-                  <div className='flex-1 flex justify-center items-center'>
-
-                  <button
-                    type="submit"
-                    className="w-full bg-green-500 py-2 rounded-md"
+                  <div className="flex justify-center mt-4">
+                    <button
+                      type="submit"
+                      className="w-full bg-green-500 py-2 rounded-md"
                     >
-                    Create Tournament
-                  </button>
-                    </div>
+                      Create Tournament
+                    </button>
+                  </div>
                 </form>
               </div>
 
-
-              <div className={`bg-gray-700/50 shadow-lg rounded-lg p-6 max-w-xs border border-violet-primary lg:min-w-[350px] min-h-[500px] mt-6 lg:mt-0 ${!confirmation && 'opacity-50 pointer-events-none'}`}>
-                <h2 className="text-lg text-center text-white font-semibold mb-4">Join Tournament</h2>
-                <div className="flex flex-col gap-2 lg:gap-6">
+              {/* Join Tournament Card */}
+              <div className="bg-gray-700/50 shadow-lg rounded-lg p-6  w-full border border-violet-primary lg:min-w-[350px] h-[500px] overflow-y-scroll mt-6 lg:mt-0">
+                <h2 className="text-lg text-center text-white font-semibold mb-10">Join Tournament</h2>
+                <div className="flex flex-col gap-4">
                   {availableTournaments.map((tournament, index) => (
                     <div
                       key={index}
-                      className="flex items-center max-w-lg space-x-2 p-2 gap-2 border rounded-lg hover:bg-gray-100 cursor-pointer"
+                      className="flex items-center w-full space-x-4 p-4 border rounded-lg hover:bg-gray-100 cursor-pointer"
                       onClick={() => handleJoinTournament(tournament)}
                     >
                       <img
--- frontend/app/src/app/(pages)/history/page.tsx
+++ frontend/app/src/app/(pages)/history/page.tsx
@@ -3,44 +3,53 @@ import Profil from "../../../../public/Profil.jpg";
 
 const MatchDetails = ({ player1, player2, score }: { player1: string; player2: string; score: string }) => {
   return (
-    <div className="flex flex-row items-center justify-between bg-gray-800/70 py-4 px-1 rounded-2xl border border-violet-primary mb-4 
-      md:px-4 lg:py-6 lg:px-6">
-      <div className="flex items-center">
+    <div className="flex flex-row items-center justify-between bg-gray-800/70 py-4  rounded-2xl border border-violet-primary mb-4
+      md:px-2 lg:py-6 lg:px-2">
+      <div className="flex items-center justify-start">
         <img
           src={Profil.src}
           alt={`${player1} Profile`}
-          className="w-10 h-10 md:w-12 md:h-12 lg:w-16 lg:h-16 rounded-full mx-1"
+          className="w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 rounded-full mx-1"
         />
-        <p className="font-thin text-white text-sm md:text-base lg:text-lg">{player1}</p>
+        <p className="font-thin text-white text-xs text-nowrap md:text-base lg:text-lg">{player1}</p>
       </div>
-      <p className="font-bold text-white text-center text-sm w-10 mx-1 md:text-base lg:text-lg">{score}</p>
-      <div className="flex items-center">
-        <p className="font-thin text-white text-sm md:text-base lg:text-lg">{player2}</p>
+      <div className="flex items-center justify-center mx-3">
+      <p className="font-bold text-white text-center text-xs w-10  md:text-base lg:text-lg">{score}</p>
+        </div>
+        <div className="flex justify-end items-center">
+        <p className="font-thin text-white text-nowrap text-xs md:text-base lg:text-lg">{player2}</p>
         <img
           src={Profil.src}
           alt={`${player2} Profile`}
-          className="w-10 h-10 md:w-12 md:h-12 lg:w-16 lg:h-16 rounded-full mx-1"
-        />
-      </div>
+          className="w-12 h-12 md:w-12 md:h-12 lg:w-16 lg:h-16 rounded-full mx-1"
+          />
+          </div>
     </div>
   );
 };
 
-const TournamentDetails = ({ title, image, matches }: { title: string; image: string; matches: any[] }) => {
+const TournamentDetails = ({ title, image, rounds }: { title: string; image: string; rounds: any[] }) => {
   return (
-    <div className="bg-gray-800/70 rounded-xl border border-violet-primary mb-6 px-2 py-2 md:px-6 md:py-4 lg:px-8 lg:py-6">
+    <div className="bg-gray-800/70 rounded-xl border border-violet-primary mb-6 h-fit py-4 md:px-2 md:py-6 lg:px-2 lg:py-6">
       <div className="flex items-center justify-center my-1 w-full">
         <img
           src={image}
           alt={`${title} Image`}
-          className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 rounded-full mr-4"
+          className="w-14 h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 rounded-full mr-4"
         />
-        <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-white">Name</h3>
+        <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-white">{title}</h3>
       </div>
+
       <div className="px-1">
-        <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-center text-white">{title}</h3>
-        {matches.map((match, index) => (
-          <MatchDetails key={index} {...match} />
+        {rounds.map((round, index) => (
+          <div key={index} className="mb-6">
+            <h4 className="text-lg md:text-xl lg:text-2xl font-semibold text-white mb-4">{round.title}</h4>
+            <div className="space-y-4">
+              {round.matches.map((match, matchIndex) => (
+                <MatchDetails key={matchIndex} {...match} />
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </div>
@@ -50,29 +59,50 @@ const TournamentDetails = ({ title, image, matches }: { title: string; image: st
 const HistoryPage = () => {
   const tournamentData = [
     {
-      title: "Semi-Finals",
+      title: "Tournament 1",
       image: Profil.src,
-      matches: [
-        { player1: "Team A", player2: "Team B", score: "8:6" },
-        { player1: "Team C", player2: "Team D", score: "10:9" },
+      rounds: [
+        {
+          title: "Semi-Finals",
+          matches: [
+            { player1: "Team A", player2: "Team B", score: "8:6" },
+            { player1: "Team C", player2: "Team D", score: "10:9" },
+          ],
+        },
+        {
+          title: "Finals",
+          matches: [{ player1: "Team A", player2: "Team C", score: "12:10" }],
+        },
       ],
     },
     {
-      title: "Finals",
+      title: "Tournament 2",
       image: Profil.src,
-      matches: [{ player1: "Team A", player2: "Team C", score: "12:10" }],
+      rounds: [
+        {
+          title: "Semi-Finals",
+          matches: [
+            { player1: "Team E", player2: "Team F", score: "9:7" },
+            { player1: "Team G", player2: "Team H", score: "11:8" },
+          ],
+        },
+        {
+          title: "Finals",
+          matches: [{ player1: "Team E", player2: "Team G", score: "13:11" }],
+        },
+      ],
     },
   ];
 
   return (
-    <div className="h-full py-10 px-4">
-      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-white text-center mb-6">History</h1>
+    <div className="h-full py-10 px-4 max-w-screen-2xl lg:min-w-full">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white text-center mb-6">History</h1>
 
-      <div className="flex flex-col md:flex-row md:space-x-4">
+      <div className="flex flex-col lg:flex-row lg:space-x-8 space-y-6 lg:space-y-0">
         {/* 1 VS 1 Section */}
-        <div className="md:w-1/2 p-4 md:p-6 lg:p-8 rounded-xl border border-violet-primary mb-6 md:mb-0">
-          <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-center text-white mb-4">1 VS 1</h2>
-          <div className="space-y-4 overflow-y-auto max-h-64 md:max-h-80 lg:max-h-96">
+        <div className="w-full lg:w-1/2 p-2 rounded-xl border border-violet-primary mb-6 lg:mb-0">
+          <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-center text-white mb-4">1 VS 1</h2>
+          <div className="space-y-4 overflow-y-auto max-h-64 lg:max-h-[30rem]">
             <MatchDetails player1="Player1" player2="Player2" score="5:3" />
             <MatchDetails player1="Player3" player2="Player4" score="6:4" />
             <MatchDetails player1="Player5" player2="Player6" score="7:5" />
@@ -80,9 +110,9 @@ const HistoryPage = () => {
         </div>
 
         {/* Tournament Section */}
-        <div className="md:w-1/2 p-4 md:p-6 lg:p-8 rounded-xl border border-violet-primary">
-          <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-center text-white mb-4">Tournament</h2>
-          <div className="space-y-6 overflow-y-auto max-h-96 md:max-h-[30rem] lg:max-h-[40rem]">
+        <div className="w-full lg:w-1/2 p-2 rounded-xl border border-violet-primary">
+          <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-center text-white mb-4">Tournaments</h2>
+          <div className="space-y-6 overflow-y-auto max-h-[30rem] lg:max-h-[40rem]">
             {tournamentData.map((tournament, index) => (
               <TournamentDetails key={index} {...tournament} />
             ))}
