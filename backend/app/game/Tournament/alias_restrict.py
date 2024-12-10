e0c94d9f20acd862851f142c3138afa0510c407b
msamhaou <tahaexo@gmail.com>
2024-12-11 00:06:36 +0100

    Fix: simple fix in Exceptions args

--- backend/app/game/Tournament/alias_restrict.py
+++ backend/app/game/Tournament/alias_restrict.py
@@ -1,14 +1,14 @@
 class AliasException(Exception):
     def __init__(self, message=None):
-        super().__init__(message=message)
+        super().__init__(message)
     pass
 
 class NoAlias(AliasException):
-    def __init__(self, message=None):
-        super().__init__(message="you have to set an alias to proceed")
+    def __init__(self, message="you have to set an alias to proceed"):
+        super().__init__(message)
     pass
 
 class AliasAlreadyUsed(AliasException):
-    def __init__(self, message=None):
-        super().__init__(message="alias already used ! Please pick another alias")
+    def __init__(self, message="alias already used ! Please pick another alias"):
+        super().__init__(message)
     pass
